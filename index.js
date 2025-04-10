2fcffd7a302cb1f385aefdf2131505ebbd87db08
wfernandezfurcal <ferfwill@gmail.com>
2025-04-09 23:57:29 -0400

    order resume feature

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
 import { menuArray } from "./data.js"
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
 
 const itemsArr = document.getElementById("items")
 const orderItems = document.getElementById("orderItems")
+const totalPrice = document.getElementById("totalPrice")
+
+let orderResume = [];
 
 document.addEventListener("click",function(e){
     if(e.target.classList[0] == 'addItemBtn'){
         addItemToOrder(e.target.id)
     }
+    else if (e.target.classList[0] == 'removeBtn'){
+        removeOrderItem(e.target.id)
+    }
 })
 
 function addItemToOrder(itemId){
 
-    const itemSelected = menuArray.filter( item => item.id == itemId)[0]
+    //orderResume == [] && 
+    //document.getElementById("orderResume").classList.toggle('hidden')
 
-    orderItems.innerHTML += 
+    const itemSelected = menuArray.filter( item => item.id == itemId)[0]
+    orderResume.push({
+        "name": itemSelected.name,
+        "price": itemSelected.price,
+        "id": uuidv4()
+    })
+
+    renderOrderResume()
+}
+
+function removeOrderItem(itemId){
+    orderResume = orderResume.filter(item => item.id !== itemId)
+    renderOrderResume()
+}
+
+function renderOrderResume(){
+    totalPrice.innerText = '$' + orderResume.reduce( (total, currentItem) => total + currentItem.price, 0 )
+    orderItems.innerHTML = orderResume.map( item =>  
                     `<div class="orderItem" id="orderItem">
                         <div>
-                            <h3>${itemSelected.name}</h3>
-                            <button class="removeBtn">remove</button>
+                            <h3>${item.name}</h3>
+                            <button class="removeBtn" id="${item.id}">remove</button>
                         </div>
-                        <h4>${itemSelected.price}</h4>                    
+                        <h4>$${item.price}</h4>               
                     </div>`
+    ).join('')
 }
 
 
